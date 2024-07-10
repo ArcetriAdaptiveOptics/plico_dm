47b5f499763f5e9673f45f2b598f43e654ce9daa
Alfio Puglisi <alfio.puglisi@gmail.com>
2024-07-10 10:02:55 +0200

    Added list_dms() and find() main functions

--- plico_dm/__init__.py
+++ plico_dm/__init__.py
@@ -13,7 +13,6 @@ def _getDefaultConfigFilePath():
 defaultConfigFilePath= _getDefaultConfigFilePath()
 
 
-
 def deformableMirror(hostname, port):
 
     from plico_dm.client.deformable_mirror_client import DeformableMirrorClient
@@ -21,8 +20,19 @@ def deformableMirror(hostname, port):
     from plico.rpc.zmq_ports import ZmqPorts
     from plico.rpc.sockets import Sockets
 
-
     rpc= ZmqRemoteProcedureCall()
     zmqPorts= ZmqPorts(hostname, port)
     sockets= Sockets(zmqPorts, rpc)
     return DeformableMirrorClient(rpc, sockets)
+
+
+def list_dms(timeout_in_seconds=2):
+    from plico.utils.discovery_server import DiscoveryClient
+    return DiscoveryClient().run(timeout_in_seconds=timeout_in_seconds, filter={'server_type':'plico_dm'})
+
+
+def find(dm_name, timeout_in_seconds=2):
+    from plico.utils.discovery_server import DiscoveryClient
+    server_info = DiscoveryClient().run(dm_name, timeout_in_seconds, filter={'server_type':'plico_dm'})
+    return deformableMirror(server_info.host, server_info.port)
+
